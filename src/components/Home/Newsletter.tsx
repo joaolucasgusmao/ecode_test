29cab821406b261a0930e91a9c3883ac6eeea248
joaolucasgusmao <joaolucasgusmao@hotmail.com>
2024-11-10 16:41:00 -0300

    style: stylized newsletter

--- src/components/Home/Newsletter.tsx
+++ src/components/Home/Newsletter.tsx
@@ -20,21 +20,30 @@ const Newsletter = () => {
 
   return (
     <>
-      <label htmlFor="emailInput">Inscreva-se em nossa newsletter</label>
-      <form onSubmit={handleSubmit}>
-        <div>
+      <form className={"w-96 h-20"} onSubmit={handleSubmit}>
+        <label htmlFor="emailInput">Inscreva-se em nossa newsletter</label>
+        <div
+          className={
+            "flex items-center justify-between w-full h-4/5"
+          }
+        >
           <input
+            className={"w-10/12 h-12 border-black border-6 rounded-md pl-4 outline-none"}
             id="emailInput"
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             required
+            placeholder="Endereço de e-mail"
           />
-          <button type="submit">
+          <button
+            className={"bg-black py-3 px-4 text-center rounded-md"}
+            type="submit"
+          >
             <Image
               src="/arrow.svg"
               alt="Botão de enviar"
-              width={15}
+              width={12}
               height={15}
             />
           </button>
