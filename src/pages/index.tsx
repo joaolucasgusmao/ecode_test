6b59bc054214b7e568be14fad720b6aedc119ed3
joaolucasgusmao <joaolucasgusmao@hotmail.com>
2024-11-09 22:41:32 -0300

    feat: Render at home created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,31 @@
-const Home = () => {};
+import ProfileCard from "@/components/commons/ProfileCard";
+import fetchProfile from "@/services/profileApi";
+import { ProfileInfos } from "@/types/HomeInfos";
+
+interface HomeProps {
+  profile: ProfileInfos;
+}
+
+const Home = ({ profile }: HomeProps) => {
+  return (
+    <>
+      <ProfileCard profile={profile} />
+    </>
+  );
+};
+
+export const getServerSideProps = async () => {
+  try {
+    const profile = await fetchProfile();
+    return {
+      props: { profile },
+    };
+  } catch (error) {
+    console.error("Erro ao buscar perfil:", error);
+    return {
+      notFound: true, 
+    };
+  }
+};
 
 export default Home;
