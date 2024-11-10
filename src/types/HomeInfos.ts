da5849eba692a18cb792b507e0efeeb3f0bfd723
joaolucasgusmao <joaolucasgusmao@hotmail.com>
2024-11-09 22:36:30 -0300

    feat: types created

---
+++ src/types/HomeInfos.ts
@@ -0,0 +1,34 @@
+export interface SocialMediaLink {
+  slug: string;
+  url: string;
+  svg_icon_jsx: string;
+}
+
+export interface ContentBlockConfig {
+  provider?: string;
+  url?: string;
+  qrcode_image_url?: string;
+  email?: string;
+}
+
+export interface ContentBlock {
+  type: string;
+  type_label: string;
+  title: string;
+  config: ContentBlockConfig;
+}
+
+export interface ShareLinks {
+  facebook: string;
+  twitter: string;
+  whatsapp: string;
+}
+
+export interface ProfileInfos {
+  name: string;
+  bio: string;
+  avatar_url: string;
+  social_media_links: SocialMediaLink[];
+  content_blocks: ContentBlock[];
+  share_links: ShareLinks;
+}
