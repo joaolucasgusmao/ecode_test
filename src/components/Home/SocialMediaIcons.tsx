41a95448232556fed289428033145923ac281d02
joaolucasgusmao <joaolucasgusmao@hotmail.com>
2024-11-10 15:24:48 -0300

    style: stylized SocialMediaIcons

--- src/components/Home/SocialMediaIcons.tsx
+++ src/components/Home/SocialMediaIcons.tsx
@@ -7,15 +7,15 @@ interface SocialMediaProps {
 
 const SocialMediaIcons = ({ socialMedia }: SocialMediaProps) => {
   return (
-    <ul>
+    <ul className={"flex gap-4 items-center my-1"}>
       {socialMedia.map((social) => {
         return (
-          <li key={social.slug}>
+          <li key={social.slug} className={"bg-black p-2 rounded-md"}>
             <Link href={social.url} target="_blank" rel="noopener noreferrer">
               <div
                 dangerouslySetInnerHTML={{ __html: social.svg_icon_jsx }}
                 aria-label={social.slug}
-                style={{ width: 20, height: 20 }}
+                style={{ width: 23, height: 23, fill: 'currentcolor', color: '#FFFFFF'}}
               />
             </Link>
           </li>
