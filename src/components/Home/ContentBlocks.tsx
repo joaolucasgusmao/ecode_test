6d64f55c4dddbba6d4084b4a8c8fe91ddbd0ca9c
joaolucasgusmao <joaolucasgusmao@hotmail.com>
2024-11-10 18:57:39 -0300

    feat: youtube modal created

--- src/components/Home/ContentBlocks.tsx
+++ src/components/Home/ContentBlocks.tsx
@@ -1,138 +1,160 @@
 import { ContentBlock } from "@/types/HomeInfos";
 import Image from "next/image";
+import { useState } from "react";
+import ThanksModal from "./ThanksModal";
 
 interface contentBlockProps {
   contentBlock: ContentBlock[];
 }
 
 const ContentBlocks = ({ contentBlock }: contentBlockProps) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [videoUrl, setVideoUrl] = useState<string | null>(null);
+
+  const openModal = (url: string) => {
+    setVideoUrl(url);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setVideoUrl(null);
+  };
+
   return (
-    <ul className={"h-60 flex gap-8 my-7"}>
-      {contentBlock.map((block) => {
-        if (block.type === "video") {
-          const thumbnailUrl = getYouTubeThumbnail(block.config.url);
+    <div>
+      <ul className={"h-60 flex gap-8 my-7"}>
+        {contentBlock.map((block) => {
+          if (block.type === "video") {
+            const thumbnailUrl = getYouTubeThumbnail(block.config.url);
 
-          return (
-            <li key={block.title} className={"relative"}>
-              {thumbnailUrl && (
+            return (
+              <li key={block.title} className={"relative"}>
+                {thumbnailUrl && (
+                  <Image
+                    src={thumbnailUrl}
+                    alt={block.title}
+                    width={453}
+                    height={100}
+                    className={"h-full rounded-xl  brightness-60"}
+                  />
+                )}
                 <Image
-                  src={thumbnailUrl}
+                  src={"/play.svg"}
                   alt={block.title}
-                  width={453}
-                  height={100}
-                  className={"h-full rounded-xl  brightness-60"}
+                  width={33.84}
+                  height={40}
+                  className={"absolute right-52 top-24 cursor-pointer"}
+                  onClick={() => openModal(block.config.url)}
                 />
-              )}
-              <Image
-                src={"/play.svg"}
-                alt={block.title}
-                width={33.84}
-                height={40}
-                className={"absolute right-52 top-24 cursor-pointer"}
-              />
-            </li>
-          );
-        }
-        if (block.type === "download") {
-          return (
-            <li
-              key={block.title}
-              className={
-                "bg-black w-48 rounded-xl flex items-center justify-center"
-              }
-            >
-              <a
-                className={"flex flex-col items-center gap-3"}
-                href={block.config.url}
-                download
-                target="_blank"
+              </li>
+            );
+          }
+          if (block.type === "download") {
+            return (
+              <li
+                key={block.title}
+                className={
+                  "bg-black w-48 rounded-xl flex items-center justify-center"
+                }
               >
-                <Image
-                  src={"/download.svg"}
-                  alt={block.type}
-                  width={28}
-                  height={28}
-                />
-                <h2 className={"text-white text-base"}>{block.type_label}</h2>
-              </a>
-            </li>
-          );
-        }
+                <a
+                  className={"flex flex-col items-center gap-3"}
+                  href={block.config.url}
+                  download
+                  target="_blank"
+                >
+                  <Image
+                    src={"/download.svg"}
+                    alt={block.type}
+                    width={28}
+                    height={28}
+                  />
+                  <h2 className={"text-white text-base"}>{block.type_label}</h2>
+                </a>
+              </li>
+            );
+          }
 
-        if (block.type === "external-link") {
-          return (
-            <li
-              key={block.title}
-              className={
-                "bg-black w-48 rounded-xl flex items-center justify-center"
-              }
-            >
-              <a
-                className={"flex flex-col items-center gap-3"}
-                href={block.config.url}
-                target="_blank"
+          if (block.type === "external-link") {
+            return (
+              <li
+                key={block.title}
+                className={
+                  "bg-black w-48 rounded-xl flex items-center justify-center"
+                }
               >
-                <Image
-                  src={"/external-link.svg"}
-                  alt={block.type}
-                  width={28}
-                  height={28}
-                />
-                <h2 className={"text-white text-base"}>{block.type_label}</h2>
-              </a>
-            </li>
-          );
-        }
+                <a
+                  className={"flex flex-col items-center gap-3"}
+                  href={block.config.url}
+                  target="_blank"
+                >
+                  <Image
+                    src={"/external-link.svg"}
+                    alt={block.type}
+                    width={28}
+                    height={28}
+                  />
+                  <h2 className={"text-white text-base"}>{block.type_label}</h2>
+                </a>
+              </li>
+            );
+          }
 
-        if (block.type === "pix") {
-          return (
-            <li
-              key={block.title}
-              className={
-                "bg-black w-48 rounded-xl flex items-center justify-center"
-              }
-            >
-              <a
-                className={"flex flex-col items-center gap-3"}
-                href={block.config.url}
+          if (block.type === "pix") {
+            return (
+              <li
+                key={block.title}
+                className={
+                  "bg-black w-48 rounded-xl flex items-center justify-center"
+                }
               >
-                <Image
-                  src={"/pix.svg"}
-                  alt={block.type}
-                  width={28}
-                  height={28}
-                />
-                <h2 className={"text-white text-base"}>{block.type_label}</h2>
-              </a>
-            </li>
-          );
-        }
+                <a
+                  className={"flex flex-col items-center gap-3"}
+                  href={block.config.url}
+                >
+                  <Image
+                    src={"/pix.svg"}
+                    alt={block.type}
+                    width={28}
+                    height={28}
+                  />
+                  <h2 className={"text-white text-base"}>{block.type_label}</h2>
+                </a>
+              </li>
+            );
+          }
 
-        if (block.type === "email") {
-          return (
-            <li
-              key={block.title}
-              className={
-                "bg-black w-48 rounded-xl flex items-center justify-center"
-              }
-            >
-              <a
-                className={"flex flex-col items-center gap-3"}
-                href={`mailto:${block.config.email}`}
+          if (block.type === "email") {
+            return (
+              <li
+                key={block.title}
+                className={
+                  "bg-black w-48 rounded-xl flex items-center justify-center"
+                }
               >
-                <Image
-                  src={"/email.svg"}
-                  alt={block.type}
-                  width={28}
-                  height={28}
-                />
-                <h2 className={"text-white text-base"}>{block.type_label}</h2>
-              </a>
-            </li>
-          );
-        }
-      })}
-    </ul>
+                <a
+                  className={"flex flex-col items-center gap-3"}
+                  href={`mailto:${block.config.email}`}
+                >
+                  <Image
+                    src={"/email.svg"}
+                    alt={block.type}
+                    width={28}
+                    height={28}
+                  />
+                  <h2 className={"text-white text-base"}>{block.type_label}</h2>
+                </a>
+              </li>
+            );
+          }
+        })}
+      </ul>
+      {/* Modal de vídeo */}
+      {isModalOpen && videoUrl && (
+        <ThanksModal videoUrl={videoUrl} onClose={closeModal} />
+      )}
+    </div>
   );
 };
 
--- src/components/Home/ThanksModal.tsx
+++ src/components/Home/ThanksModal.tsx
@@ -1,11 +1,72 @@
-const ThanksModal = () => {
+import { useEffect, useState } from "react";
+
+const ThanksModal = ({
+  videoUrl,
+  onClose,
+}: {
+  videoUrl: string;
+  onClose: () => void;
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    setIsOpen(true);
+
+    const handleClickOutside = (event: MouseEvent) => {
+      const modal = document.getElementById("modal");
+      if (modal && !modal.contains(event.target as Node)) {
+        onClose();
+      }
+    };
+
+    const handleEscKey = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        onClose();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener("keydown", handleEscKey);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("keydown", handleEscKey);
+    };
+  }, [onClose]);
+
   return (
-    <div>
-      <div>
-        <iframe>
-            
-        </iframe>
+    <div
+      className={
+        "fixed inset-0 bg-black bg-opacity-80 flex justify-center items-center backdrop-blur-sm"
+      }
+    >
+      <div
+        id="modal"
+        className={`bg-transparent rounded-xl p-4 relative max-w-4xl flex flex-col transition-transform transform ${
+          isOpen ? "translate-y-0" : "-translate-y-full"
+        }`}
+      >
+        <button
+          onClick={onClose}
+          className={"absolute top-6 right-6 text-white text-xl"}
+        >
+          X
+        </button>
+        <iframe
+          width="600"
+          height="315"
+          src={`https://www.youtube.com/embed/${getYouTubeVideoId(videoUrl)}`}
+          frameBorder="0"
+          allowFullScreen
+          className={"rounded-xl"}
+        />
       </div>
     </div>
   );
 };
+
+const getYouTubeVideoId = (url: string) => {
+  return url.split("v=")[1]?.split("&")[0];
+};
+
+export default ThanksModal;
