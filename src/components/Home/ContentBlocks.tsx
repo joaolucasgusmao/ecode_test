cdabec00280936dd7e6de8f12f6ef6ddfc77dcba
joaolucasgusmao <joaolucasgusmao@hotmail.com>
2024-11-10 23:32:36 -0300

    fix: ContentBlock HTML changed

--- src/components/Home/ContentBlocks.tsx
+++ src/components/Home/ContentBlocks.tsx
@@ -30,131 +30,151 @@ const ContentBlocks = ({ contentBlock }: contentBlockProps) => {
   };
 
   return (
-    <div>
-      <ul className={"h-60 flex gap-8 my-7"}>
-        {contentBlock.map((block) => {
-          if (block.type === "video") {
-            const thumbnailUrl = getYouTubeThumbnail(block.config.url);
+    <div className={"flex gap-4 my-8 h-60"}>
+      {contentBlock.map((block) => {
+        if (block.type === "video") {
+          const thumbnailUrl = getYouTubeThumbnail(block.config.url);
 
-            return (
-              <li key={block.title} className={"relative"}>
-                {thumbnailUrl && (
-                  <Image
-                    src={thumbnailUrl}
-                    alt={block.title}
-                    width={453}
-                    height={100}
-                    className={"h-full rounded-xl  brightness-60"}
-                  />
-                )}
+          return (
+            <li key={block.title} className={"relative list-none"}>
+              {thumbnailUrl && (
                 <Image
-                  src={"/play.svg"}
+                  src={thumbnailUrl}
                   alt={block.title}
-                  width={33.84}
-                  height={40}
-                  className={"absolute right-52 top-24 cursor-pointer"}
-                  onClick={() => openModal(block.config.url)}
+                  width={450}
+                  height={100}
+                  className={"h-full rounded-xl  brightness-60"}
                 />
-              </li>
-            );
-          }
-          if (block.type === "download") {
-            return (
-              <li
-                key={block.title}
-                className={
-                  "bg-black w-48 rounded-xl flex items-center justify-center"
-                }
-              >
-                <a
-                  className={"flex flex-col items-center gap-3"}
-                  href={block.config.url}
-                  download
-                  target="_blank"
-                >
-                  <Image
-                    src={"/download.svg"}
-                    alt={block.type}
-                    width={28}
-                    height={28}
-                  />
-                  <h2 className={"text-white text-base"}>{block.type_label}</h2>
-                </a>
-              </li>
-            );
-          }
+              )}
+              <Image
+                src={"/play.svg"}
+                alt={block.title}
+                width={33.84}
+                height={40}
+                className={"absolute right-48 top-24 cursor-pointer"}
+                onClick={() => openModal(block.config.url)}
+              />
+            </li>
+          );
+        }
+      })}
 
-          if (block.type === "external-link") {
-            return (
-              <li
-                key={block.title}
-                className={
-                  "bg-black w-48 rounded-xl flex items-center justify-center"
-                }
-              >
-                <a
-                  className={"flex flex-col items-center gap-3"}
-                  href={block.config.url}
-                  target="_blank"
+      <ul className={"flex gap-4"}>
+        {contentBlock.map((block) => {
+          if (
+            block.type === "download" ||
+            block.type === "external-link" ||
+            block.type === "pix" ||
+            block.type === "email"
+          ) {
+            if (block.type === "download") {
+              return (
+                <li
+                  key={block.title}
+                  className={
+                    "bg-black w-48 rounded-xl flex items-center justify-center"
+                  }
                 >
-                  <Image
-                    src={"/external-link.svg"}
-                    alt={block.type}
-                    width={28}
-                    height={28}
-                  />
-                  <h2 className={"text-white text-base"}>{block.type_label}</h2>
-                </a>
-              </li>
-            );
-          }
+                  <a
+                    className={"flex flex-col items-center gap-3"}
+                    href={block.config.url}
+                    download
+                    target="_blank"
+                  >
+                    <Image
+                      src={"/download.svg"}
+                      alt={block.type}
+                      width={28}
+                      height={28}
+                    />
+                    <h2 className={"text-white text-base"}>
+                      {block.type_label}
+                    </h2>
+                  </a>
+                </li>
+              );
+            }
 
-          if (block.type === "pix") {
-            return (
-              <li
-                key={block.title}
-                className={
-                  "bg-black w-48 rounded-xl flex items-center justify-center"
-                }
-              >
-                <a
-                  className={"flex flex-col items-center gap-3 cursor-pointer"}
-                  onClick={() => openPixModal(block.config.qrcode_image_url)}
+            if (block.type === "external-link") {
+              return (
+                <li
+                  key={block.title}
+                  className={
+                    "bg-black w-48 rounded-xl flex items-center justify-center"
+                  }
                 >
-                  <Image
-                    src={"/pix.svg"}
-                    alt={block.type}
-                    width={28}
-                    height={28}
-                  />
-                  <h2 className={"text-white text-base"}>{block.type_label}</h2>
-                </a>
-              </li>
-            );
-          }
+                  <a
+                    className={"flex flex-col items-center gap-3"}
+                    href={block.config.url}
+                    target="_blank"
+                  >
+                    <Image
+                      src={"/external-link.svg"}
+                      alt={block.type}
+                      width={28}
+                      height={28}
+                    />
+                    <h2 className={"text-white text-base"}>
+                      {block.type_label}
+                    </h2>
+                  </a>
+                </li>
+              );
+            }
 
-          if (block.type === "email") {
-            return (
-              <li
-                key={block.title}
-                className={
-                  "bg-black w-48 rounded-xl flex items-center justify-center"
-                }
-              >
-                <a
-                  className={"flex flex-col items-center gap-3"}
-                  href={`mailto:${block.config.email}`}
+            if (block.type === "pix") {
+              return (
+                <li
+                  key={block.title}
+                  className={
+                    "bg-black w-48 rounded-xl flex items-center justify-center"
+                  }
                 >
-                  <Image
-                    src={"/email.svg"}
-                    alt={block.type}
-                    width={28}
-                    height={28}
-                  />
-                  <h2 className={"text-white text-base"}>{block.type_label}</h2>
-                </a>
-              </li>
-            );
+                  <a
+                    className={
+                      "flex flex-col items-center gap-3 cursor-pointer"
+                    }
+                    onClick={() => openPixModal(block.config.qrcode_image_url)}
+                  >
+                    <Image
+                      src={"/pix.svg"}
+                      alt={block.type}
+                      width={28}
+                      height={28}
+                    />
+                    <h2 className={"text-white text-base"}>
+                      {block.type_label}
+                    </h2>
+                  </a>
+                </li>
+              );
+            }
+
+            if (block.type === "email") {
+              return (
+                <li
+                  key={block.title}
+                  className={
+                    "bg-black w-48 rounded-xl flex items-center justify-center"
+                  }
+                >
+                  <a
+                    className={"flex flex-col items-center gap-3"}
+                    href={`mailto:${block.config.email}`}
+                  >
+                    <Image
+                      src={"/email.svg"}
+                      alt={block.type}
+                      width={28}
+                      height={28}
+                    />
+                    <h2 className={"text-white text-base"}>
+                      {block.type_label}
+                    </h2>
+                  </a>
+                </li>
+              );
+            }
           }
         })}
       </ul>
